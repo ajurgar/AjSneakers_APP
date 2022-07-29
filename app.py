c79bfa42b176aa654245ddfd8583dbec6fc04e9c
Alejandro Garcia <alejandrojg8@gmail.com>
2022-07-29 17:08:49 +0100

    start routes and templates

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
+from controllers.products_controller import products_blueprint
+
 app= Flask(__name__)
+
+app.register_blueprint(products_blueprint)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
